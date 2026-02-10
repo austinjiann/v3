563c6724c5bb7dc0b2521ed71094abd8fe4c89b4
Austin Jian <austin.jian07@gmail.com>
2026-02-10 18:10:51 -0500

    chore: unhadcode year 🤦

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,7 @@ import { WebringBadge } from '@/components/webringbage';
 import * as motion from 'motion/react-client';
 
 export function Footer() {
+  const year = new Date().getFullYear();
   return (
     <motion.footer 
       className="w-full flex items-center justify-between pt-8 pb-4"
@@ -11,7 +12,7 @@ export function Footer() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5, delay: 0.8 }}
     >
-      <p className="text-gray-400 text-sm">© austin jian 2026</p>
+      <p className="text-gray-400 text-sm">© austin jian {year}</p>
       <WebringBadge siteUrl="austinjian.ca" />
     </motion.footer>
   );
